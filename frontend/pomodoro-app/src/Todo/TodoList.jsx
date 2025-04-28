f00b5a3db84d45264e186617150fb1f22d31c9b8
seominhee <minheee07@gmail.com>
2025-04-28 23:56:14 +0900

    투두 컴포넌트 생성

--- frontend/pomodoro-app/src/Todo/TodoList.jsx
+++ frontend/pomodoro-app/src/Todo/TodoList.jsx
@@ -1,7 +1,20 @@
+import React, { useState }from "react";
+import style from "../css/Todo/Todo.module.scss";
+
 function TodoList() {
+    const [inputTodo, setInputTodo] = useState("");
+
+    const onChange = (e) => {
+
+    }
+
     return(
         <div>
-            투두
+            <input type="text" onChange={onChange} className={style.inputTodo} placeholder="할 일을 기록하세요"></input>
+            {/* 투두<br></br>
+            타이머 숫자 60 이상 안넘게<br></br>
+            타이머 끝나면 알람 가도록<br></br>
+            뽀모도로 튜토리얼<br></br> */}
         </div>
     );
 }
