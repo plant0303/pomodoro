7dd7b19b9088dfba790438dbb3c99da4ff12eff4
seominhee <minheee07@gmail.com>
2025-04-30 00:12:10 +0900

    투두리스트 입력받기

--- frontend/pomodoro-app/src/Todo/TodoList.jsx
+++ frontend/pomodoro-app/src/Todo/TodoList.jsx
@@ -1,38 +1,62 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import global from "../global.scss"
 import style from "../css/Todo/Todo.module.scss";
 
 function TodoList() {
     const [inputTodo, setInputTodo] = useState("");
-    const [todoList, setTodoList] = useState("");
-    // 할일 입력받기
-    const onChange = (e) => {
+    const [todoList, setTodoList] = useState([
+        { id: 1, todo: "할일1" },
+        { id: 2, todo: "할일2" },
+        { id: 3, todo: "할일3" }
+    ]);
 
+    console.log(todoList);
+    const activeEnter = (e) => {
+        if (e.key === "Enter" && inputTodo.trim() !== "") {
+            const newTodoList = {
+                id: todoList.length + 1,
+                todo: inputTodo.trim()
+            };
+
+            setTodoList([...todoList, newTodoList]);
+            setInputTodo("");
+        }
     }
 
     // 투두 리스트 출력하기
-
-    return(
+    const printTodo = todoList.map((todo) => {
+        return (
+            <li>
+                <input type="checkbox" 
+                id={`todo-${todo.id}`} 
+                className={style.screenReader} />
+                <div className={style.labelBox}>
+                    <span className={style.checkIcon} aria-hidden="true"></span>
+                    <label for={`todo-${todo.id}`}>{todo.todo}</label>
+                </div>
+                <div className={style.menu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </li>
+        );
+    });
+    return (
         <>
             <h2 className={style.todoTitle}>Todo</h2>
             <div className={style.todoCont}>
-                <input type="text" onChange={onChange} className={style.inputTodo} placeholder="할 일을 기록하세요"></input>
+                <input type="text"
+                    value={inputTodo} 
+                    onChange={(e) => setInputTodo(e.target.value)}
+                    onKeyDown={(e) => activeEnter(e)}
+                    className={style.inputTodo}
+                    placeholder="할 일을 기록하세요"></input>
                 <div className={style.todoList}></div>
             </div>
             <div className={style.todoList}>
                 <ul>
-                    <li>
-                        <input type="checkbox" id="todo1" className={style.screenReader}/>
-                        <div className={style.labelBox}>
-                            <span className={style.checkIcon} aria-hidden="true"></span>
-                            <label for="todo1">할일</label>
-                        </div>
-                        <div className={style.menu}>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </li>
+                    {printTodo}
                 </ul>
             </div>
             {/* 투두<br></br>
