7db29e24b6e98dd0836ee41e9e034430436ce3d0
seominhee <minheee07@gmail.com>
2025-04-30 01:45:22 +0900

    수정 삭제 버튼 출력

--- frontend/pomodoro-app/src/Todo/TodoList.tsx
+++ frontend/pomodoro-app/src/Todo/TodoList.tsx
@@ -8,17 +8,18 @@ interface Todo {
 }
 
 function TodoList() {
-    
+
     const [inputTodo, setInputTodo] = useState("");
     const [todoList, setTodoList] = useState([
         { id: 1, todo: "할일1" },
         { id: 2, todo: "할일2" },
         { id: 3, todo: "할일3" }
     ]);
-    const [openMenuId, setOpenMenuID] = useState(null); // 두투 메뉴 오픈 상태
+    const [openMenuId, setOpenMenuId] = useState<number | null>(null); // 두투 메뉴 오픈 상태
 
+    console.log(openMenuId);
 
-    console.log(todoList);
+    //투두 입력받기
     const activeEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter" && inputTodo.trim() !== "") {
             const newTodoList = {
@@ -31,32 +32,49 @@ function TodoList() {
         }
     }
 
+    // 투두 리스트 햄버거 메뉴 출력
+    const toggleMunu = (id: number) => {
+        setOpenMenuId((prevId) => (prevId === id ? null : id));
+    }
     // 투두 리스트 출력하기
     const printTodo = todoList.map((todo) => {
         return (
-            <li>
-                <input type="checkbox" 
-                id={`todo-${todo.id}`} 
-                className={style.screenReader} />
+            <li className={style.todoLi}>
+                <input type="checkbox"
+                    id={`todo-${todo.id}`}
+                    className={style.screenReader} />
                 <div className={style.labelBox}>
                     <span className={style.checkIcon} aria-hidden="true"></span>
                     <label htmlFor={`todo-${todo.id}`}>{todo.todo}</label>
                 </div>
-                <div className={style.menu}>
+                <div className={style.menu}
+                    onClick={() => toggleMunu(todo.id)}
+                    tabIndex={0}
+                    role="button">
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
+                {openMenuId === todo.id &&
+                    <div className={style.menuPopup}>
+                        <ul>
+                            <li><button>수정</button></li>
+                            <li><button>삭제</button></li>
+                        </ul>
+                    </div>
+                }
             </li>
         );
     });
+
+
 
     return (
         <>
             <h2 className={style.todoTitle}>Todo</h2>
             <div className={style.todoCont}>
                 <input type="text"
-                    value={inputTodo} 
+                    value={inputTodo}
                     onChange={(e) => setInputTodo(e.target.value)}
                     onKeyDown={(e) => activeEnter(e)}
                     className={style.inputTodo}
