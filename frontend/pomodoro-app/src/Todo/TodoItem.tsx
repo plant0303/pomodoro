06abe75214e29f06e1771a9d41983411e9eafc74
seominhee <minheee07@gmail.com>
2025-05-20 23:15:19 +0900

    여백을 눌러서 수정을 완료

--- frontend/pomodoro-app/src/Todo/TodoItem.tsx
+++ frontend/pomodoro-app/src/Todo/TodoItem.tsx
@@ -17,30 +17,55 @@ function TodoItem({ todo, onDelete, onUpdate, isEditing, startEditing, stopEditi
     const [updateTodo, setUpdateTodo] = useState<string>(todo.todo); // 투두 수정
     const inputRef = useRef<HTMLInputElement>(null); // 수정 input에 자동 포커스
     const pastTodo = todo.todo;
+    const itemRef = useRef<HTMLLIElement | null>(null);
 
-    const handleUpdate = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // 수정 저장 로직
+    const handleSave = () => {
+        const trimmed = updateTodo.trim();
+
+        if (trimmed === "") {
+            onUpdate(todo.id, pastTodo); // 원래 값으로 복구
+        } else {
+            onUpdate(todo.id, trimmed);  // 수정된 값 저장
+        }
+
+        stopEditing(); // 공통 처리
+
+    }
+
+    // enter 입력시 저장
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            const trimmed = updateTodo.trim();
-
-            if (trimmed === "") {
-                onUpdate(todo.id, pastTodo); // 원래 값으로 복구
-            } else {
-                onUpdate(todo.id, trimmed);  // 수정된 값 저장
-            }
-
-            stopEditing(); // 공통 처리
+            handleSave();
         }
     }
 
+    // 여백 클릭시 저장
+    useEffect(() => {
+        if (!isEditing) return;
+
+        const handleClickOutside = (e: MouseEvent) => {
+            if (
+                itemRef.current &&
+                !itemRef.current.contains(e.target as Node)
+            ) {
+                handleSave();
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.addEventListener("mousedown", handleClickOutside);
+    });
+
     // 수정영역 input 자동 포커스
     useEffect(() => {
-        if(isEditing && inputRef.current){
+        if (isEditing && inputRef.current) {
             inputRef.current.focus();
         }
     }, [isEditing]);
 
     return (
-        <li key={`todo${todo.id}`} className={style.todoLi}>
+        <li ref={itemRef} key={`todo${todo.id}`} className={style.todoLi}>
             <input type="checkbox"
                 id={`todo-${todo.id}`}
                 className={style.screenReader} />
@@ -50,7 +75,7 @@ function TodoItem({ todo, onDelete, onUpdate, isEditing, startEditing, stopEditi
                     <input type="text"
                         onChange={(e) => setUpdateTodo(e.target.value)}
                         value={updateTodo}
-                        onKeyDown={(e) => handleUpdate(e)}
+                        onKeyDown={handleKeyDown}
                         ref={inputRef}></input>
                 </div> :
                 <div className={style.labelBox}>
@@ -58,7 +83,7 @@ function TodoItem({ todo, onDelete, onUpdate, isEditing, startEditing, stopEditi
                     <label htmlFor={`todo-${todo.id}`}>{todo.todo}</label>
                 </div>
             }
-            <TodoMenu todoId={todo.id} setIsEditing={startEditing} onDelete={onDelete} listRef={listRef}/>
+            <TodoMenu todoId={todo.id} setIsEditing={startEditing} onDelete={onDelete} listRef={listRef} />
         </li>
     );
 }
